f43a171d5acaba93f87d89bbe43833772196e654
baohuynh0188 <baohuynh0188@outlook.com>
2025-10-12 21:59:51 +0700

    refactor: remove JiraTimesheetCaptureStack and associated Lambda functions for ticket management

--- bin/elevensys-cdk.ts
+++ bin/elevensys-cdk.ts
@@ -2,7 +2,6 @@
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 import * as cdk from 'aws-cdk-lib';
-import { JiraTimesheetCaptureStack } from '../lib/stacks/jira-timesheet-capture-stack';
 import { JiraTimesheetUiStack } from '../lib/stacks/jira-timesheet-ui-stack';
 import { UrlifyStack } from '../lib/stacks/urlify-stack';
 
@@ -16,12 +15,6 @@ const env = {
   account: process.env.CDK_DEFAULT_ACCOUNT || '',
   region: process.env.CDK_DEFAULT_REGION || '',
 };
-
-// Configuration for API domain
-const apiDomainName = process.env.API_DOMAIN_NAME;
-const apiHostedZoneId = process.env.API_HOSTED_ZONE_ID;
-const apiCertificateArn = process.env.API_CERTIFICATE_ARN;
-const apiDomainPrefix = process.env.API_DOMAIN_PREFIX || 'api';
 
 const redirectDomain = process.env.REDIRECT_DOMAIN_NAME || '';
 const adminDomain = process.env.ADMIN_DOMAIN_NAME || '';
@@ -42,15 +35,6 @@ if (!urlifyCertificateArn) {
 const uiDomainName = process.env.UI_DOMAIN_NAME;
 const uiHostedZoneId = process.env.UI_HOSTED_ZONE_ID;
 const uiCertificateArn = process.env.UI_CERTIFICATE_ARN;
-
-// Deploy the backend API stack with CloudFront and custom domain
-new JiraTimesheetCaptureStack(app, 'JiraTimesheetCaptureStack', {
-  env,
-  domainName: apiDomainName,
-  apiDomainPrefix,
-  hostedZoneId: apiHostedZoneId,
-  certificateArn: apiCertificateArn,
-});
 
 // Deploy the static UI stack backed by S3 + CloudFront
 new JiraTimesheetUiStack(app, 'JiraTimesheetUiStack', {
